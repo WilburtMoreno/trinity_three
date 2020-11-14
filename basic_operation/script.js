5d436cd5ddff67d986ed6e8e608e6e0ae3e71ba2
Wilburt Moreno <wilburtmoreno1@gmail.com>
2020-11-13 20:19:27 -0800

    Update script.js

--- basic_operation/script.js
+++ basic_operation/script.js
@@ -4,63 +4,94 @@ const scoreLimit = document.getElementById('limit-score');
 const operand = document.getElementById('operand');
 const operator = document.getElementById('operator');
 const answer = document.getElementById('solve-it');
+const operation = document.getElementById('operation');
 
-const arrOperation = ['*', '+', '-'];
+const arrOperation = ['*', '/', '+', '-'];
 
 let points = 0;
+let limit = 10;
 const userAns = answer.value;
 
 // Problem random numbers
-let randOperand = Math.floor(Math.random() * 10) + 1; 
-let randOperator = Math.floor(Math.random() * 10 ) + 1;
-let operationSelect = Math.floor(Math.random() * arrOperation.length);
+let randOperand;
+let randOperator;
+let operationSelect;
 
-const correctAns = ans(randOperand, randOperator, arrOperation);
+// correct answer for the problem
+let correctAns
+
+// UI //
+getValues();
+problem(); 
+scoreLimit.value = limit;
 
 //      TEST        //
 // console.log(ans(randOperand, randOperator, arrOperation));
 // console.log(arrOperation);
 // console.log(arrOperation[operationSelect]);
-  console.log(randOperand + arrOperation[operationSelect] +  randOperator);
-//  console.log(eval(randOperand + arrOperation[operationSelect] + randOperator));
+// console.log(randOperand + arrOperation[operationSelect] +  randOperator);
+//   console.log(eval(randOperand + arrOperation[operationSelect] + randOperator));
 
 // function 
-function ans(operand, operator, operation) {
-    return eval(operand + operation[operationSelect] + operator);
-}
+function calcOperation(operand, operator, operation) {
+    
+    // The index of divide is 1
+    if(operationSelect == 1) {
+         return  console.log((eval(parseFloat(operand + operation[operationSelect] + operator)).toFixed(2)).toString());
 
-if(userAns == correctAns) {
-    points++;
+        
+    } else {
+        return (eval(operand + operation[operationSelect] + operator)).toString();
+    }
+   
 }
-
-// score limit 
-if(score == scoreLimit) {
-    alert('Finish');
+function problem() {
+    operand.innerText = randOperand;
+    operator.innerText = randOperator;
+    operation.innerText = arrOperation[operationSelect];
+}
+function getValues() {
+    randOperand = randNumber();
+    randOperator = randNumber()
+    operationSelect = Math.floor(Math.random() * arrOperation.length);
+    correctAns = calcOperation(randOperand, randOperator, arrOperation);
+}
+function randNumber() {
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 // EventListener
 answer.addEventListener('keyup', e => {
+    console.log('correct answer:');
+    console.log(parseInt(correctAns));
+
     if(e.keyCode === 13) {
-        console.log('enter');
-        const answer = parseInt(e.target.value);
+        const userAnswer = e.target.value;
   
-        if(answer === correctAns) {
-            console.log('correct');
+        // if answer is correct or wrong
+        if(userAnswer === correctAns) {
+            console.log('Answer: ');
+            console.log(userAnswer);
+            answer.classList.remove('incorrect');
+            getValues();
+            problem();
+            answer.value = '';
+
+            points++;
+            score.innerText = points;
+        } else {
+            console.log('Answer ');
+            console.log(userAnswer);
+            answer.classList.add('incorrect');
         }
+
+         if (parseInt(scoreLimit.value) === parseInt(score.textContent)) {
+             alert('Finish');
+
+             getValues();
+             problem();
+             answer.value = '';
+             points = 0;
+         }
     }
-})
-
-
-// // Get the input field
-// var input = document.getElementById("myInput");
-
-// // Execute a function when the user releases a key on the keyboard
-// input.addEventListener("keyup", function(event) {
-//   // Number 13 is the "Enter" key on the keyboard
-//   if (event.keyCode === 13) {
-//     // Cancel the default action, if needed
-//     event.preventDefault();
-//     // Trigger the button element with a click
-//     document.getElementById("myBtn").click();
-//   }
-// });
+});
